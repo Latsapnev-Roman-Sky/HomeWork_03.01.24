ffce67fa02f01843a175b4eb144f021e815309c0
latsa <latsapnev01@mail.ru>
2024-01-02 17:38:29 +0300

    Task №2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static int[] generateRandomArray() {
@@ -21,6 +22,23 @@ public class Main {
             sum = sum + i;
         }
         System.out.println("Сумма трат за месяц "+sum);
+    }
+    public static void task2() {
+        int[] array = generateRandomArray();
+        System.out.println(Arrays.toString(array));
+        int minSpendAmount = array[0];
+        int maxSpendAmount = array[0];
+        for (final int currentSpendAmount : array){
+            if (currentSpendAmount <= minSpendAmount){
+                minSpendAmount = currentSpendAmount;
+            }
+            if (currentSpendAmount >= maxSpendAmount){
+                maxSpendAmount = currentSpendAmount;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила "+minSpendAmount+" рублей");
+        System.out.println("Максимальная сумма трат за день составила "+maxSpendAmount+" рублей");
+
     }
 }
 
