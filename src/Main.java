5f12b39450d8d4e1e8176408a3572ebb258c3c88
latsa <latsapnev01@mail.ru>
2024-01-02 17:36:16 +0300

    Task №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        int[] array = generateRandomArray();
+        System.out.println(Arrays.toString(array));
+        int sum = 0;
+        for (int i : array) {
+            sum = sum + i;
+        }
+        System.out.println("Сумма трат за месяц "+sum);
     }
 }
+
+
+
